c65455500d2f43e8358016e543d8699ad65fc1c1
Araqelyan Tiran <tirans3777@gmail.com>
2019-04-08 23:13:14 +0400

    Add solve for exercises 2

--- C-SharpProgrammingExercises/ExsSolution/ExeCodeSignal.cs
+++ C-SharpProgrammingExercises/ExsSolution/ExeCodeSignal.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 
 namespace ExsSolution
 {
@@ -10,6 +6,7 @@ namespace ExsSolution
     {
         public static int[][] Revers(int[][] matric)
         {
+            //     Exercises 1
             //    You are given an n x n 2D matrix that represents an image.
             //    Rotate the image by 90 degrees(clockwise).
             if (matric.LongCount() == 1) return matric;
@@ -23,6 +20,26 @@ namespace ExsSolution
             }
 
             return @new;
+        }
+        //     Exercises 2
+        //    Ratiorg got statues of different sizes as a present from CodeMaster for his birthday, 
+        //    each statue having an non-negative integer size. Since he likes to make things perfect, 
+        //    he wants to arrange them from smallest to largest so that each statue will be bigger than the previous one exactly by 1. 
+        //    He may need some additional statues to be able to accomplish that. 
+        //    Help him figure out the minimum number of additional statues needed.
+        
+        public static int MakeArrayConsecutive2(int[] statues)
+        {
+            int count = 0;
+            int min = statues.Min();
+            int max = statues.Max();
+            while (max != min)
+            {
+                min++;
+                if (!statues.Contains(min)) count++;
+
+            }
+            return count;
         }
     }
 }
