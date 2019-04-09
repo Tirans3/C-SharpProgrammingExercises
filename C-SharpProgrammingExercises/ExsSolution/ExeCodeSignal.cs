c747b795793e1eba26e93de5ce34fbc21061f906
Araqelyan Tiran <tirans3777@gmail.com>
2019-04-10 00:30:19 +0400

    Add solve for Exercises 3

--- C-SharpProgrammingExercises/ExsSolution/ExeCodeSignal.cs
+++ C-SharpProgrammingExercises/ExsSolution/ExeCodeSignal.cs
@@ -1,5 +1,5 @@
-﻿using System.Linq;
-
+﻿using System.Collections.Generic;
+using System.Linq;
 namespace ExsSolution
 {
     class ExeCodeSignal
@@ -27,7 +27,7 @@ namespace ExsSolution
         //    he wants to arrange them from smallest to largest so that each statue will be bigger than the previous one exactly by 1. 
         //    He may need some additional statues to be able to accomplish that. 
         //    Help him figure out the minimum number of additional statues needed.
-        
+
         public static int MakeArrayConsecutive2(int[] statues)
         {
             int count = 0;
@@ -40,6 +40,36 @@ namespace ExsSolution
 
             }
             return count;
+        }
+        //     Exercises 3
+        // Given a sequence of integers as an array, determine 
+        // whether it is possible to obtain a strictly increasing sequence
+        // by removing no more than one element from the array.
+
+        //Note: sequence a0, a1, ..., an is considered to be a strictly
+        //increasing if a0<a1< ... < an.Sequence containing only one 
+        //element is also considered to be strictly increasing.
+
+        public static bool Issort(int[] a)
+        {
+            bool s = false;
+
+            bool IsSorted(int[] b)
+            {
+                bool x = true;
+                b.Aggregate((d, c) => { x = x && d < c; return c; });
+                return x;
+            }
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                List<int> temp = a.ToList();
+                temp.RemoveAt(i);
+                s = s || IsSorted(temp.ToArray());
+
+            }
+
+            return s;
         }
     }
 }
